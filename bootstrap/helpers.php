523d35945940988fae7d3fa640edb24c937f4f47
Sheldon <454177882@qq.com>
2017-08-18 17:27:08 +0800

    Updated database configuration

---
+++ bootstrap/helpers.php
@@ -0,0 +1,29 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: hp
+     * Date: 2017/8/18
+     * Time: 17:21
+     */
+    function get_db_config()
+    {
+        if (getenv('IS_IN_HEROKU')) {
+            $url = parse_url(getenv("DATABASE_URL"));
+
+            return $db_config = [
+                'connetion' => 'pgsql',
+                'host' => $url["host"],
+                'database'  => substr($url["path"], 1),
+                'username'  => $url["user"],
+                'password'  => $url["pass"],
+            ];
+        } else {
+            return $db_config = [
+                'connetion' => env('DB_CONNECTION', 'mysql'),
+                'host' => env('DB_HOST', 'localhost'),
+                'database'  => env('DB_DATABASE', 'forge'),
+                'username'  => env('DB_USERNAME', 'forge'),
+                'password'  => env('DB_PASSWORD', ''),
+            ];
+        }
+    }
